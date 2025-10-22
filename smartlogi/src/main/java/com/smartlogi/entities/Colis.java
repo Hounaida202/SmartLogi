cf881f9e005003528eb00b1a98fb15afbc2702e6
Hounaida202 <etrichih@gmail.com>
2025-10-22 12:25:53 +0100

    la creation des classes colis et livreur avec leurs attributs et leurs getters et setters

--- smartlogi/src/main/java/com/smartlogi/entities/Colis.java
+++ smartlogi/src/main/java/com/smartlogi/entities/Colis.java
@@ -1,9 +1,92 @@
-package com.smartlogi.Entities;
+package com.smartlogi.entities;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 @Entity
-@
 public class Colis {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_colis;
+
+    private String destination;
+    private String adresse;
+    private double poids;
+
+    public enum Status {
+        PREPARE, TRANSIT, LIVRE
+    }
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    // Relation : plusieurs colis pour un livreur
+    @ManyToOne
+    @JoinColumn(name = "id_livreur") //  crée la clé étrangère dans la table Colis
+    private Livreur livreur;
+
+    public Colis() {}
+
+    public Colis(String destination, String adresse, double poids, Status status, Livreur livreur) {
+        this.destination = destination;
+        this.adresse = adresse;
+        this.poids = poids;
+        this.status = status;
+        this.livreur = livreur;
+    }
+
+    public int getId_colis() {
+        return id_colis;
+    }
+
+    public void setId_colis(int id_colis) {
+        this.id_colis = id_colis;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Livreur getLivreur() {
+        return livreur;
+    }
+
+    public void setLivreur(Livreur livreur) {
+        this.livreur = livreur;
+    }
 }
--- smartlogi/src/main/java/com/smartlogi/entities/Livreur.java
+++ smartlogi/src/main/java/com/smartlogi/entities/Livreur.java
@@ -1,4 +1,93 @@
-package com.smartlogi.Entities;
+package com.smartlogi.entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "livreurs")
 public class Livreur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_livreur;
+
+    private String nom;
+    private String prenom;
+    private String vehicule;
+    private long tele;
+
+    //  Un livreur peut avoir plusieurs colis
+    @OneToMany(mappedBy = "livreur", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Colis> colisList;
+
+    //  Constructeurs
+    public Livreur() {}
+
+    public Livreur(int id_livreur, String nom, String prenom, String vehicule, long tele) {
+        this.id_livreur = id_livreur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.vehicule = vehicule;
+        this.tele = tele;
+    }
+
+    //  Getters et setters
+    public int getId_livreur() {
+        return id_livreur;
+    }
+
+    public void setId_livreur(int id_livreur) {
+        this.id_livreur = id_livreur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(String vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public long getTele() {
+        return tele;
+    }
+
+    public void setTele(long tele) {
+        this.tele = tele;
+    }
+
+    public List<Colis> getColisList() {
+        return colisList;
+    }
+
+    public void setColisList(List<Colis> colisList) {
+        this.colisList = colisList;
+    }
+
+    @Override
+    public String toString() {
+        return "Livreur{" +
+                "id_livreur=" + id_livreur +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", vehicule='" + vehicule + '\'' +
+                ", tele=" + tele +
+                '}';
+    }
 }
